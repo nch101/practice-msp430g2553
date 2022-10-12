6b0480df57ff3e15e088c8045cdacbeecc13a7e1
Huy Nguyen Cong <huynguyencong98@gmail.com>
2022-10-12 18:14:01 +0700

    Added input and interrupt func

--- libs/gpio/gpio.c
+++ libs/gpio/gpio.c
@@ -5,14 +5,48 @@
 static GPIO_TypeDef        *GPIOx;
 static GPIO_InitTypeDef    GPIO_Init;
 
-static void GPIO_OutputInit()
+static void GPIO_OutputInit_void()
 {
     GPIOx->PxDIR |= GPIO_Init.Pin;
 };
 
-static void GPIO_InputInit()
+static void GPIO_InputInit_void()
 {
+    GPIOx->PxDIR &= ~(GPIO_Init.Pin);
 
+    switch (GPIO_Init.Pull)
+    {
+        case GPIO_PULLUP:
+            GPIOx->PxREN |= GPIO_Init.Pin;
+            GPIOx->PxOUT |= GPIO_Init.Pin;
+            break;
+
+        case GPIO_PULLDOWN:
+            GPIOx->PxREN |= GPIO_Init.Pin;
+            GPIOx->PxOUT &= ~(GPIO_Init.Pin);
+            break;
+
+        case GPIO_NOPULL:
+        default:
+            break;
+    }
+};
+
+static void GPIO_InterruptRisingEdgeInit_void()
+{
+    GPIOx->PxIE     |= GPIO_Init.Pin;
+    GPIOx->PxIES    &= ~(GPIO_Init.Pin);
+};
+
+static void GPIO_InterruptFallingEdgeInit_void()
+{
+    GPIOx->PxIE     |= GPIO_Init.Pin;
+    GPIOx->PxIES    |= GPIO_Init.Pin;
+};
+
+static uint8_t isInterruptPending()
+{
+    // To do: Check interrupt flag vs Interrupt pin
 };
 
 void GPIO(GPIO_TypeDef *_GPIOx, GPIO_InitTypeDef _GPIO_Init)
@@ -23,16 +57,22 @@ void GPIO(GPIO_TypeDef *_GPIOx, GPIO_InitTypeDef _GPIO_Init)
     switch (GPIO_Init.Mode)
     {
         case GPIO_MODE_INPUT:
-            {
-                GPIO_InputInit();
-                break;
-            }
+            GPIO_InputInit_void();
+            break;
 
         case GPIO_MODE_OUTPUT:
-            {
-                GPIO_OutputInit();
-                break;
-            }
+            GPIO_OutputInit_void();
+            break;
+
+        case GPIO_MODE_IT_RISING:
+            GPIO_InterruptRisingEdgeInit_void();
+            GPIO_InputInit_void();
+            break;
+
+        case GPIO_MODE_IT_FALLING:
+            GPIO_InterruptFallingEdgeInit_void();
+            GPIO_InputInit_void();
+            break;
 
         default:
             break;
@@ -47,4 +87,17 @@ void GPIO_setOutputSignal_void(GPIO_State_u8 GPIO_State)
 void GPIO_toggleOutputSignal_void()
 {
     GPIOx->PxOUT ^= GPIO_Init.Pin;
+};
+
+#pragma vector = PORT1_VECTOR
+__interrupt void GPIO_handlingPort1IRQ_void()
+{
+    // To do: Call process interrupt function
+};
+
+void __attribute__((weak)) GPIO_processingPortIRQ_void();
+
+void GPIO_processingPortIRQ_void()
+{
+    // Add comment
 };
